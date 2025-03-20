afc19b9d84aab5a4ceb3b6cfebfe0001f347b74f
gracian-ruiz <gracianmiguel1995@gmail.com>
2025-03-20 22:16:56 +0100

    menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,12 +19,6 @@
                         <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
                             Usuarios
                         </x-nav-link>
-                        <x-nav-link :href="route('bikes.index')" :active="request()->routeIs('bikes.index')">
-                            Bicicletas
-                        </x-nav-link>
-                        <x-nav-link :href="route('components.index')" :active="request()->routeIs('components.*')">
-                            Componentes
-                        </x-nav-link>
                         <x-nav-link :href="route('presupuestos.index')" :active="request()->routeIs('presupuestos.index')">
                             Presupuestos
                         </x-nav-link>
@@ -33,6 +27,12 @@
                         </x-nav-link>  
                         <x-nav-link :href="route('revisions.index')" :active="request()->routeIs('revisions.*')">
                             Revisiones
+                        </x-nav-link>
+                        <x-nav-link :href="route('bikes.index')" :active="request()->routeIs('bikes.index')">
+                            Bicicletas
+                        </x-nav-link>
+                        <x-nav-link :href="route('components.index')" :active="request()->routeIs('components.*')">
+                            Componentes
                         </x-nav-link>
                         <x-nav-link :href="route('avisos.index')" :active="request()->routeIs('avisos.*')">
                             Mensajes
