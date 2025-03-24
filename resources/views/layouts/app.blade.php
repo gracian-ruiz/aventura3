2a12a8020063ccf9856b44bec42283d73e27f546
gracian-ruiz <gracianmiguel1995@gmail.com>
2025-03-24 17:25:28 +0100

    tablet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,10 +34,10 @@
 
         <!-- Page Content -->
         <main class="flex-grow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-4 md:px-3 lg:px-8 text-base sm:text-sm md:text-xs">
                 @yield('content')
             </div>
-        </main>
+        </main>        
     </div>
     @yield('scripts')
 </body>
