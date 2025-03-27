a711158391150e62971dafae53177f9aa9bbed69
gracian-ruiz <gracianmiguel1995@gmail.com>
2025-03-27 14:40:59 +0100

    estado citas

--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -37,7 +37,7 @@
                     <th class="py-2 px-4 text-left">Prioridad</th>
                     <th class="py-2 px-4 text-left">Tiempo Estimado</th>
                     <th class="py-2 px-4 text-left">Fecha Creación</th>
-                    <th class="py-2 px-4 text-left">Fecha Asignada</th>
+{{--                     <th class="py-2 px-4 text-left">Fecha Asignada</th> --}}
 {{--                     <th class="py-2 px-4 text-left">Descripción Problema</th> --}}
                     <th class="py-2 px-4 text-left">Estado</th>
                     <th class="py-2 px-4 text-center">Acciones</th>
@@ -70,7 +70,7 @@
                             {{ $appointment->fecha_asignada ? \Carbon\Carbon::parse($appointment->fecha_asignada)->format('d/m/Y') : 'Pendiente' }}
                         </td>
                        {{--  <td class="py-2 px-4">{{ $appointment->descripcion_problema ?? 'N/A' }}</td> --}}
-                        <td class="py-2 px-4">{{ ucfirst($appointment->estado) }}</td>
+                        {{-- <td class="py-2 px-4">{{ ucfirst($appointment->estado) }}</td> --}}
                         <td class="py-2 px-4 text-center">
                             <!-- Botón Revisar (solo si está pendiente) -->
                             @if($appointment->estado == 'pendiente')
