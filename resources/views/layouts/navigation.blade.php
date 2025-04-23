f72ee45b024a96cfd5fa1e3f1b53fc9a0f84c184
gracian-ruiz <gracianmiguel1995@gmail.com>
2025-04-23 12:36:56 +0200

    orden de taller

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -65,7 +65,8 @@
             @if(Auth::user()->role === 'admin')
                 <x-responsive-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">Usuarios</x-responsive-nav-link>
                 <x-responsive-nav-link :href="route('presupuestos.index')" :active="request()->routeIs('presupuestos.index')">Presupuestos</x-responsive-nav-link>
-                <x-responsive-nav-link :href="route('appointments.index')" :active="request()->routeIs('appointments.index')">Citas</x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('appointments.index')" :active="request()->routeIs('appointments.index')">Orden de taller
+                </x-responsive-nav-link>
                 <x-responsive-nav-link :href="route('revisions.index')" :active="request()->routeIs('revisions.*')">Revisiones</x-responsive-nav-link>
                 <x-responsive-nav-link :href="route('bikes.index')" :active="request()->routeIs('bikes.index')">Bicicletas</x-responsive-nav-link>
                 <x-responsive-nav-link :href="route('components.index')" :active="request()->routeIs('components.*')">Componentes</x-responsive-nav-link>
