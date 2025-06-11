e4f7762c3e2cfff7f580863d89debfae6c7c0ede
gracian-ruiz <gracianmiguel1995@gmail.com>
2025-06-11 16:27:15 +0200

    cambios

--- routes/web.php
+++ routes/web.php
@@ -263,10 +263,10 @@ Route::post('/confirmacion/presupuesto/{presupuestoId}', [PresupuestoController:
 
 
 
-Route::get('/alquiler/bicicletas/montana', [AventuraBikeController::class, 'bicismontaña'])
+Route::get('/alquiler/bicicletas', [AventuraBikeController::class, 'bicismontaña'])
     ->name('bicismontaña');
 
-Route::post('/alquiler/bicicletas', [AventuraBikeController::class, 'store'])->middleware('throttle:5,1')->name('addbicismontaña');
+Route::post('/alquiler/bicicletas/montana', [AventuraBikeController::class, 'store'])->middleware('throttle:5,1')->name('addbicismontaña');
 
 Route::post('/verificar-disponibilidad', [
     AventuraBikeController::class, 'comprobarDisponibilidad'])->name('bicicletas.disponibilidad');
