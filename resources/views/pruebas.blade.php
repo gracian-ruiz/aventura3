caab6e1709cd3571e900e0a25163aff1a912c678
gracian-ruiz <gracianmiguel1995@gmail.com>
2025-07-08 22:25:21 +0200

    xxx3

--- resources/views/pruebas.blade.php
+++ resources/views/pruebas.blade.php
@@ -673,8 +673,12 @@
 <div class="fade-in portada" style="height: 770px">
   <div class="fade-in portada-contenido">
     <!-- Imagen de fondo -->
+        <img class="imagen-borde" src="{{ asset('images/boda/photo-border-2.png') }}" alt="borde decorativo" style="margin-bottom: 418px;
+    width: 790px!important;
+    margin-left: -161px;transform: rotate(185deg);">
+
     <img style="margin-bottom: 216px;background: #eae1d2;
-    padding-bottom: 50px;" class="imagen-base" src="{{ asset('images/boda/anillo2.jpeg') }}" alt="foto pareja">
+    padding-bottom: 50px;padding-top: 20px;" class="imagen-base" src="{{ asset('images/boda/anillo2.jpeg') }}" alt="foto pareja">
 tan especial
     <!-- Imagen con borde roto transparente -->
     <img class="imagen-borde" src="{{ asset('images/boda/photo-border-2.png') }}" alt="borde decorativo" style="margin-bottom: 17px;
