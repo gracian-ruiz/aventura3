ecf431584c28d9fe95a610a5fe59730687876f4b
gracian-ruiz <gracianmiguel1995@gmail.com>
2025-07-08 21:09:48 +0200

    xxxx34

--- resources/views/pruebas.blade.php
+++ resources/views/pruebas.blade.php
@@ -579,7 +579,7 @@
 
   <!-- Texto inferior -->
   <div class="texto-inferior" style="width: 390px;height: 84px">
-    <p style="margin-left: -18px;" class="adios fadee-out  fecha">— 18.10.2025 —</p>
+    <p style="margin-left: -28px;" class="adios fadee-out  fecha">— 18.10.2025 —</p>
   </div>
 </div>
 
