cf9249ba41d9cc416077890b54aa49ae121e4322
gracian-ruiz <gracianmiguel1995@gmail.com>
2025-08-21 12:36:47 +0200

    cambios

--- resources/views/pruebas.blade.php
+++ resources/views/pruebas.blade.php
@@ -9,7 +9,8 @@
 
 
 
-<a href="https://wa.me/34637319765?text=Hola%20quiero%20más%20informaci%C3%B3n" 
+<a href="https://wa.me/34637319765?text=%F0%9F%93%84%20%C2%A1Hola%20PEDRO%20MARIA%20LAZARO%20JIMENEZ%20DE%20CISNEROS!%20Te%20escribo%20de%20Aventura%20Bike,%20te%20env%C3%ADo%20el%20presupuesto%20para%20arreglar%20tu%20bicicleta%20'E-FOLDER'.%0A%0A%F0%9F%93%8E%20Adjuntamos%20el%20PDF%20con%20los%20detalles.%0A%0A%F0%9F%94%97%20Puedes%20confirmar%20el%20presupuesto%20pinchando%20aqu%C3%AD:%20si%20no%20est%C3%A1s%20de%20acuerdo%20dime%20qu%C3%A9%20quieres%20que%20hagamos%20y%20te%20mando%20nuevo%20presupuesto.%20Gracias:%20https%3A%2F%2Ftaller.aventurabike.es%2Fpublic%2Fconfirmacion%2Fpresupuesto%2F159%3Ftoken%3Dbf66a2f62e1ca3513ae0b976b5e5ebb4
+" 
    target="_blank" 
    style="background:#25D366; color:white; padding:10px 15px; border-radius:5px; text-decoration:none; font-weight:bold;">
    📲 Escríbenos por WhatsApp
