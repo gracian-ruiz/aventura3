76ff92daafab225fae95c597e5180431d450ce07
gracian-ruiz <gracianmiguel1995@gmail.com>
2025-11-30 20:07:09 +0100

    correo

--- app/Mail/ReservaAlquilerMail.php
+++ app/Mail/ReservaAlquilerMail.php
@@ -25,16 +25,40 @@ class ReservaAlquilerMail extends Mailable
         $this->observaciones = $observaciones;
     }
     
-    
-
     public function build()
     {
+        // Mapeo de tipos a nombres bonitos
+        $tipos = [
+            'mtb26' => 'MTB 26',
+            'mtb29' => 'MTB 29',
+            'mtb29doble' => 'MTB 29 Doble',
+            'electricapaseo' => 'Eléctrica Paseo',
+            'electricadoble' => 'Eléctrica Doble',
+            'electricarigida' => 'Eléctrica Rígida',
+            'carretera' => 'Carretera',
+            'paseo' => 'Paseo',
+            'niños' => 'Niños',
+            'ninos' => 'Niños',
+            'casco' => 'Casco',
+            'material' => 'Material',
+            'bidones' => 'Bidones',
+            'reparacion' => 'Reparación',
+            'bombin' => 'Bombín',
+            'kit_reparacion' => 'Kit Reparación',
+        ];
+        // Convertir el tipo de cada bicicleta a nombre bonito
+        $bicicletas = collect($this->bicicletas)->map(function($bici) use ($tipos) {
+            $bici['tipo_bonito'] = $tipos[$bici['tipo']] ?? $bici['tipo'];
+            return $bici;
+        });
         return $this->subject('Confirmación de reserva de bicicletas')
                     ->bcc(['aventurabikepk@gmail.com']) 
-                    ->view('emails.reserva') // creamos este archivo en el paso siguiente
+                    ->view('emails.reserva')
                     ->with([
                         'alquiler' => $this->alquiler,
                         'usuario' => $this->usuario,
+                        'bicicletas' => $bicicletas,
+                        'observaciones' => $this->observaciones,
                     ]);
     }
 }
